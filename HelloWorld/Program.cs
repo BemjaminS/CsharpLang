f7567b1b3a3871f15eac173b81a533d4f0f18f53
BenJamin <Bswisa@yahoo.com>
2022-05-30 17:55:35 +0300

    Update File

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,6 +8,35 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("My First Step In C#");
+
+            //variable in C#
+
+            //Cannot start variable with number e.g -> 1route
+            //while we declare variable recomended to use meaningful names
+
+            //naming convention:
+            //Camel Case -> firstName
+
+            //Pascal Case -> FirstName
+
+            //Hungarian Case -> strFirstName
+
+            //For local variables: Camel Case
+            int number;
+
+            //For constant variable: Pascal Case
+
+            const float Pi = 3.14f;
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
